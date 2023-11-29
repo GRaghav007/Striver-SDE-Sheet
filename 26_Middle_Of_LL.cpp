33e7fae32ce073c46c05550f1e2830d2bddbbe12
GRaghav007 <118236827+GRaghav007@users.noreply.github.com>
2023-11-29 23:40:37 +0530

    Add files via upload

---
+++ 26_Middle_Of_LL.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast -> next){
+            slow = slow -> next;
+            fast = fast -> next;
+            if(fast -> next)    fast = fast -> next;
+        }
+
+        return slow;
+    }
+};
